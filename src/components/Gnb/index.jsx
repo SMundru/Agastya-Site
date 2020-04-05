60365649eee2fd1c42c3252b45d5c3f46f702079
Sivan Mundru <sivan.mundru@leadx.com>
2020-04-05 12:21:38 +0100

    cleanup

--- src/components/Gnb/index.jsx
+++ src/components/Gnb/index.jsx
@@ -1,8 +1,8 @@
-import React, {useCallback, useContext, useEffect, useReducer} from 'react';
+import React, { useCallback, useContext, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
-import {Link, navigate} from 'gatsby';
+import { Link } from 'gatsby';
 import Toggle from 'react-toggle';
-import {FaCaretDown, FaSearch} from 'react-icons/fa';
+import { FaCaretDown, FaSearch } from 'react-icons/fa';
 import {
   Background,
   GnbWrapper,
@@ -19,7 +19,7 @@ import {
   SubMenu,
   ToggleWrapper,
 } from './styled';
-import {myContext} from "../App/AppContext";
+import { myContext } from '../App/AppContext';
 
 const TOGGLE_MENU = 'TOGGLE_MENU';
 const TOGGLE_CAT_SUB_MENU = 'TOGGLE_CAT_SUB_MENU';
@@ -61,7 +61,7 @@ const reducer = (state = initialState, action) => {
       };
     }
     case INPUT_KEYWORD: {
-      const {searchKeyword} = action;
+      const { searchKeyword } = action;
 
       return {
         ...state,
@@ -74,47 +74,46 @@ const reducer = (state = initialState, action) => {
 };
 
 const Gnb = ({
-               location,
-               toggleTheme,
-               isDracula
-             }) => {
+  location,
+  toggleTheme,
+  isDracula,
+}) => {
   const context = useContext(myContext);
-  if (!context.dataLoaded) {
-    fetch(`https://l27wt82pxc.execute-api.eu-west-2.amazonaws.com/dev/videos/details`)
-        .then(response => response.json())
-        .then(resultData => {
+  if (typeof context !== 'undefined') {
+    const { dataLoaded } = context;
+    if (dataLoaded) {
+      fetch('https://l27wt82pxc.execute-api.eu-west-2.amazonaws.com/dev/videos/details')
+        .then((response) => response.json())
+        .then((resultData) => {
           context.setDataLoaded();
-          const body = resultData.body;
+          const { body } = resultData;
           context.setCategories(body.categories);
           context.setYears(body.years);
           context.setCategoryVideoMap(body.categoryVideoMap);
           context.setYearVideoMap(body.yearVideoMap);
-        })
+        });
+    }
   }
-  const [{isMenuOpened, isCatSubMenuClosed, isAgeSubMenuClosed, searchKeyword}, dispatch] = useReducer(reducer, initialState);
+  const [{ isMenuOpened, isCatSubMenuClosed, isAgeSubMenuClosed, searchKeyword }, dispatch] = useReducer(reducer, initialState);
   const toggleMenu = useCallback(() => {
-    dispatch({type: TOGGLE_MENU});
+    dispatch({ type: TOGGLE_MENU });
   }, []);
   const toggleCatSubMenu = useCallback(() => {
-    dispatch({type: TOGGLE_CAT_SUB_MENU});
+    dispatch({ type: TOGGLE_CAT_SUB_MENU });
   }, []);
   const toggleAgeSubMenu = useCallback(() => {
-    dispatch({type: TOGGLE_AGE_SUB_MENU});
-  }, []);
-  const navigateToPath = useCallback((path) => {
-    navigate(path);
+    dispatch({ type: TOGGLE_AGE_SUB_MENU });
   }, []);
   const inputKeyword = useCallback((e) => {
     const searchKeyword = e.target.value;
 
-    dispatch({type: INPUT_KEYWORD, searchKeyword});
+    dispatch({ type: INPUT_KEYWORD, searchKeyword });
   });
 
-  const {pathname} = location;
-  const isVideos = pathname.replace(/\/$/, '')==='/videos';
+  const { pathname } = location;
+  const isVideos = pathname.replace(/\/$/, '') === '/videos';
   const isHome = pathname.replace(/\/$/, '') === '';
   const isAbout = pathname.replace(/\/$/, '') === '/about';
-  const isBlog = !(isVideos || isHome || isAbout);
 
   useEffect(() => {
     if (isMenuOpened) {
@@ -124,221 +123,210 @@ const Gnb = ({
     }
   }, [isMenuOpened]);
 
-  const categories = context.categories;
-  const years = context.years;
+  let categories = [];
+  let years = [];
+
+  if (typeof context !== 'undefined') {
+    categories = context.categories;
+    years = context.years;
+  }
   return (
-      <myContext.Consumer>
-        {() => (
-            <GnbWrapper>
-              <MobileMenu isActive={isMenuOpened}>
-                <Background onClick={toggleMenu} isActive={isMenuOpened}/>
-                <MobileMenus>
-                  <ul>
-                    <ListMenu>
-                      <StyledLink to="/" onClick={toggleMenu}>
-                        <Home/>
-                      </StyledLink>
-                    </ListMenu>
-                    <ListMenu>
-                      <StyledLink to="/about" className={isAbout ? 'active' : ''} onClick={toggleMenu}>
-                        About
-                      </StyledLink>
-                    </ListMenu>
-                    <ListMenu>
-              <span onClick={toggleCatSubMenu}>
-                Categories
-              </span>{categories.length > 0
-                        ? (<>
-                          &nbsp;
-                          <MovableFaCaretDown
-                              className={isCatSubMenuClosed ? 'is-active' : ''}
-                              onClick={toggleCatSubMenu}
-                          />
-                        </>)
-                        : null}
-                      <SubMenu isActive={isCatSubMenuClosed}>
-                        <div>
-                          {categories.map(folder => {
-                            if (folder === '') {
-                              return null;
-                            }
-                            return (
-                                <li key={folder}>
-                                  <Link to={`/videos`} onClick={toggleMenu} state={{folder: folder, division: 'category'}}>
-                                    {folder}
-                                    &nbsp;
-                                  </Link>
-                                </li>
-                            );
-                          })}
-                        </div>
-                      </SubMenu>
-                    </ListMenu>
-                    <ListMenu>
-              <span onClick={toggleAgeSubMenu}>
-                Age
-              </span>{years.length > 0
-                        ? (<>
-                          &nbsp;
-                          <MovableFaCaretDown
-                              className={isAgeSubMenuClosed ? 'is-active' : ''}
-                              onClick={toggleAgeSubMenu}
-                          />
-                        </>)
-                        : null}
-                      <SubMenu isActive={isAgeSubMenuClosed}>
-                        <div>
-                          {years.map(folder => {
-                            if (folder === '') {
-                              return null;
-                            }
-                            return (
-                                <li key={folder}>
-                                  <Link to={`/videos`} onClick={toggleMenu} state={{folder: folder, division: 'age'}}>
-                                    {folder}
-                                    &nbsp;
-                                  </Link>
-                                </li>
-                            );
-                          })}
-                        </div>
-                      </SubMenu>
-                    </ListMenu>
-                    <SearchBarWrapper>
-                      <label htmlFor="search">
-                        <FaSearch/>
-                      </label>
-                      <SearchBar
-                          id="search"
-                          type="text"
-                          value={searchKeyword}
-                          onChange={inputKeyword}
-                      />
-                    </SearchBarWrapper>
-                    {/*<SearchedPosts isEmpty={filteredPosts.length === 0}>
-              {filteredPosts.map(({ path, title, summary, tags }) => (
-                <SearchedPost key={path}>
-                  <Title onClick={() => { navigateToPath(path); }}>
-                    {title}
-                  </Title>
-                  <Summary onClick={() => { navigateToPath(path); }}>
-                    {summary}
-                  </Summary>
-                  {tags.length > 0 ? (
-                    <FaTags />
-                  ) : null}
-                  {[...new Set(tags)].map(tag => (
-                    <Tag key={tag} onClick={() => { navigateToPath(`/tags/${tag}/1`); }}>
-                      <small>
-                        {tag}
-                      </small>
-                    </Tag>
-                  ))}
-                </SearchedPost>
-              ))}
-            </SearchedPosts>*/}
-                  </ul>
-                </MobileMenus>
-              </MobileMenu>
-              <ToggleWrapper>
-                <Toggle
-                    defaultChecked={isDracula}
-                    icons={{
-                      checked: <span role="img" aria-label="change-theme">🌙</span>,
-                      unchecked: <span role="img" aria-label="change-theme">☀️</span>,
-                    }}
-                    onChange={toggleTheme}
-                />
-              </ToggleWrapper>
-              <Hamburger
-                  className={`hamburger hamburger--spin js-hamburger ${isMenuOpened ? 'is-active' : ''}`}
-                  onClick={toggleMenu}
-              >
-                <div className="hamburger-box">
-                  <div className="hamburger-inner"/>
-                </div>
-              </Hamburger>
-              <List>
+    <myContext.Consumer>
+      {() => (
+        <GnbWrapper>
+          <MobileMenu isActive={isMenuOpened}>
+            <Background onClick={toggleMenu} isActive={isMenuOpened} />
+            <MobileMenus>
+              <ul>
                 <ListMenu>
-                  <StyledLink to="/">
-                    <Home/>
+                  <StyledLink to="/" className={isHome ? 'active' : ''} onClick={toggleMenu}>
+                    <Home />
                   </StyledLink>
                 </ListMenu>
                 <ListMenu>
-                  <StyledLink to="/about" className={isAbout ? 'active' : ''}>
+                  <StyledLink to="/about" className={isAbout ? 'active' : ''} onClick={toggleMenu}>
                     About
                   </StyledLink>
                 </ListMenu>
                 <ListMenu>
-          <span className={isVideos ? 'active' : ''}>
-            Categories
-            &nbsp;
-            {categories.length > 0 ? <FaCaretDown/> : null}
-          </span>
-                  <SubMenu>
+                  <span role="button" onClick={toggleCatSubMenu} onKeyDown={toggleCatSubMenu} tabIndex={0}>
+                    Categories
+                  </span>
+                  {categories.length > 0
+                    ? (
+                      <>
+                          &nbsp;
+                        <MovableFaCaretDown
+                          className={isCatSubMenuClosed ? 'is-active' : ''}
+                          onClick={toggleCatSubMenu}
+                        />
+                      </>
+                    )
+                    : null}
+                  <SubMenu isActive={isCatSubMenuClosed}>
                     <div>
-                      {categories.map((folder, i) => {
+                      {categories.map((folder) => {
                         if (folder === '') {
                           return null;
                         }
                         return (
-                            <li key={i}>
-                              <StyledLink to={`/videos`} key={i} state={{folder: folder, division: 'category'}}>
-                                {folder}
-                                &nbsp;
-                              </StyledLink>
-                            </li>
+                          <li key={folder}>
+                            <Link to="/videos" onClick={toggleMenu} state={{ folder, division: 'category' }}>
+                              {folder}
+                                    &nbsp;
+                            </Link>
+                          </li>
                         );
                       })}
                     </div>
                   </SubMenu>
                 </ListMenu>
                 <ListMenu>
-          <span>
-            Age
-            &nbsp;
-            {years.length > 0 ? <FaCaretDown/> : null}
-          </span>
-                  <SubMenu>
+                  <span role="button" onClick={toggleAgeSubMenu} onKeyDown={toggleCatSubMenu} tabIndex={0}>
+                    Age
+                  </span>
+                  {years.length > 0
+                    ? (
+                      <>
+                          &nbsp;
+                        <MovableFaCaretDown
+                          className={isAgeSubMenuClosed ? 'is-active' : ''}
+                          onClick={toggleAgeSubMenu}
+                        />
+                      </>
+                    )
+                    : null}
+                  <SubMenu isActive={isAgeSubMenuClosed}>
                     <div>
-                      {years.map((folder, i) => {
+                      {years.map((folder) => {
                         if (folder === '') {
                           return null;
                         }
                         return (
-                            <li key={i}>
-                              <StyledLink to={`/videos`} key={i} state={{folder: folder, division: 'age'}}>
-                                {folder}
-                                &nbsp;
-                              </StyledLink>
-                            </li>
+                          <li key={folder}>
+                            <Link to="/videos" onClick={toggleMenu} state={{ folder, division: 'age' }}>
+                              {folder}
+                                    &nbsp;
+                            </Link>
+                          </li>
                         );
                       })}
                     </div>
                   </SubMenu>
                 </ListMenu>
-              </List>
-              <SearchBarWrapper>
-                <label htmlFor="search">
-                  <FaSearch/>
-                </label>
-                <SearchBar
+                <SearchBarWrapper>
+                  <label htmlFor="search">
+                    <FaSearch />
+                  </label>
+                  <SearchBar
                     id="search"
                     type="text"
                     value={searchKeyword}
                     onChange={inputKeyword}
-                />
-              </SearchBarWrapper>
-            </GnbWrapper>)
-        }
-      </myContext.Consumer>
+                  />
+                </SearchBarWrapper>
+              </ul>
+            </MobileMenus>
+          </MobileMenu>
+          <ToggleWrapper>
+            <Toggle
+              defaultChecked={isDracula}
+              icons={{
+                checked: <span role="img" aria-label="change-theme">🌙</span>,
+                unchecked: <span role="img" aria-label="change-theme">☀️</span>,
+              }}
+              onChange={toggleTheme}
+            />
+          </ToggleWrapper>
+          <Hamburger
+            className={`hamburger hamburger--spin js-hamburger ${isMenuOpened ? 'is-active' : ''}`}
+            onClick={toggleMenu}
+          >
+            <div className="hamburger-box">
+              <div className="hamburger-inner" />
+            </div>
+          </Hamburger>
+          <List>
+            <ListMenu>
+              <StyledLink to="/">
+                <Home />
+              </StyledLink>
+            </ListMenu>
+            <ListMenu>
+              <StyledLink to="/about" className={isAbout ? 'active' : ''}>
+                About
+              </StyledLink>
+            </ListMenu>
+            <ListMenu>
+              <span className={isVideos ? 'active' : ''}>
+                Categories
+                &nbsp;
+                {categories.length > 0 ? <FaCaretDown /> : null}
+              </span>
+              <SubMenu>
+                <div>
+                  {categories.map((folder) => {
+                    if (folder === '') {
+                      return null;
+                    }
+                    return (
+                      <li key={folder}>
+                        <StyledLink to="/videos" key={folder} state={{ folder, division: 'category' }}>
+                          {folder}
+                                &nbsp;
+                        </StyledLink>
+                      </li>
+                    );
+                  })}
+                </div>
+              </SubMenu>
+            </ListMenu>
+            <ListMenu>
+              <span>
+                Age
+                &nbsp;
+                {years.length > 0 ? <FaCaretDown /> : null}
+              </span>
+              <SubMenu>
+                <div>
+                  {years.map((folder) => {
+                    if (folder === '') {
+                      return null;
+                    }
+                    return (
+                      <li key={folder}>
+                        <StyledLink to="/videos" key={folder} state={{ folder, division: 'age' }}>
+                          {folder}
+                                &nbsp;
+                        </StyledLink>
+                      </li>
+                    );
+                  })}
+                </div>
+              </SubMenu>
+            </ListMenu>
+          </List>
+          <SearchBarWrapper>
+            <label htmlFor="search">
+              <FaSearch />
+            </label>
+            <SearchBar
+              id="search"
+              type="text"
+              value={searchKeyword}
+              onChange={inputKeyword}
+            />
+          </SearchBarWrapper>
+        </GnbWrapper>
+      )}
+    </myContext.Consumer>
   );
 };
 
 Gnb.propTypes = {
-  location: PropTypes.shape({pathname: PropTypes.string.isRequired}).isRequired,
+  location: PropTypes.shape({ pathname: PropTypes.string.isRequired }).isRequired,
   toggleTheme: PropTypes.func.isRequired,
-  isDracula: PropTypes.bool.isRequired
+  isDracula: PropTypes.bool.isRequired,
 };
 
 export default Gnb;
--- src/pages/videos.jsx
+++ src/pages/videos.jsx
@@ -1,60 +1,73 @@
-import React, {useContext, useEffect} from "react";
-import VideoFrame from "../components/VideoFrame";
-import {Col, Row} from "react-bootstrap";
-import {Helmet} from "react-helmet";
-import {PREFIX} from "../constants";
-import Layout from "../components/layout";
-import PostsWrapper from "../components/Common/PostsWrapper";
-import Pagination from "../components/Common/Pagination";
-import {PostContent} from "../components/Post/styled";
-import {myContext} from "../components/App/AppContext";
-import {navigate} from 'gatsby';
+import React, { useContext, useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Helmet } from 'react-helmet';
+import { navigate } from 'gatsby';
+import PropTypes from 'prop-types';
+import VideoFrame from '../components/VideoFrame';
+import { PREFIX } from '../constants';
+import Layout from '../components/layout';
+import PostsWrapper from '../components/Common/PostsWrapper';
+import Pagination from '../components/Common/Pagination';
+import { PostContent } from '../components/Post/styled';
+import { myContext } from '../components/App/AppContext';
 
-const Videos = ({location}) => {
-    const {folder, division} = location.state;
-    const context = useContext(myContext);
-    let videos = [];
+const Videos = ({ location }) => {
+  let folder; let division; let route;
+  if (typeof location !== 'undefined' && typeof location.state !== 'undefined') {
+    ({ folder, division } = location.state);
+    route = `${folder.toUpperCase()}`;
+  }
+  const context = useContext(myContext);
 
+  let videos = [];
+
+  if (typeof context !== 'undefined') {
     if (division === 'age') {
-        videos = context.yearVideoMap[folder];
+      videos = context.yearVideoMap[folder];
     } else if (division === 'category') {
-        videos = context.categoryVideoMap[folder];
+      videos = context.categoryVideoMap[folder];
     }
+  }
 
-    useEffect( () => {
-        if(typeof videos === 'undefined' || videos.length === 0) {
-            return navigate('/');
-        }
-    });
-    return (
-        <myContext.Consumer>{
-            () =>
-            <Layout location={location}>
-                <Helmet>
-                    <title>
-                        {`${PREFIX} VIDEOS | ${folder.toUpperCase()}`}
-                    </title>
-                    <meta name="og:title" content={`${PREFIX}${folder.toUpperCase()}`}/>
-                </Helmet>
-                <PostsWrapper>
-                    <Row>
-                        {typeof videos !== 'undefined' && videos.map((object, i) => {
-                            if (object === typeof 'undefined') {
-                                return null;
-                            }
-                            return (
-                                <Col key={i}>
-                                    <PostContent>
-                                        <VideoFrame detail={object}/>
-                                    </PostContent>
-                                </Col>
-                            );
-                        })}
-                    </Row>
-                </PostsWrapper>
-                <Pagination postCount={1} location={location}/>
-            </Layout>}
-        </myContext.Consumer>)
+  useEffect(() => {
+    if (typeof videos === 'undefined' || videos.length === 0) {
+      return navigate('/');
+    } return null;
+  });
+
+  return (
+    <myContext.Consumer>
+      <Layout location={location}>
+        <Helmet>
+          <title>
+            {`${PREFIX} VIDEOS | ${route}`}
+          </title>
+          <meta name="og:title" content={`${PREFIX}${route}`} />
+        </Helmet>
+        <PostsWrapper>
+          <Row>
+            {typeof videos !== 'undefined' && videos.map((object) => {
+              if (typeof object === 'undefined') {
+                return null;
+              }
+              return (
+                <Col key={object.S3Name.S}>
+                  <PostContent>
+                    <VideoFrame detail={object} />
+                  </PostContent>
+                </Col>
+              );
+            })}
+          </Row>
+        </PostsWrapper>
+        <Pagination postCount={1} location={location} />
+      </Layout>
+    </myContext.Consumer>
+  );
+};
+
+Videos.propTypes = {
+  location: PropTypes.shape({ state: PropTypes.object }).isRequired,
 };
 
 export default Videos;
