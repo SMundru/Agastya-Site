64246a4a1029eda3f94ca6d83ce31944f8ebe13f
Sivan Mundru <sivan.mundru@leadx.com>
2020-04-07 20:46:31 +0100

    delete dead code

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,41 +1,13 @@
-import React, { Children, cloneElement } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
 import App from '~/components/App';
 
 const Layout = ({ children, location }) => (
-  <StaticQuery
-    query={graphql`
-      query GatsbyQuery {
-        posts: allMarkdownRemark(
-          filter: { frontmatter: { hide: { ne: true } } }
-        ){
-          edges {
-            node {
-              frontmatter {
-                path
-                type
-                title
-                category
-                summary
-                tags
-                images
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={() => {
-      return (
-        <App
-          location={location}
-        >
-          {children}
-        </App>
-      );
-    }}
-  />
+  <App
+    location={location}
+  >
+    {children}
+  </App>
 );
 
 Layout.propTypes = {
